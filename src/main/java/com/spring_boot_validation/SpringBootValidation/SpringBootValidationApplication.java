fcb0c92369b718567abae0e7aeb85a655ec8f69e
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-09-06 23:41:41 +0530

    Adding all the initial files for Spring-Boot-MVC data validation.

---
+++ src/main/java/com/spring_boot_validation/SpringBootValidation/SpringBootValidationApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boot_validation.SpringBootValidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootValidationApplication.class, args);
+	}
+
+}
