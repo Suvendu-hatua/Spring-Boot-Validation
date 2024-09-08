cdf0451b601e3e825dec89f6ae3aa4ff2375eb58
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-09-08 20:22:57 +0530

    Handling leading and trimming spaces from input validation using @InitBinder.

--- src/main/java/com/spring_boot_validation/SpringBootValidation/controllers/CustomerController.java
+++ src/main/java/com/spring_boot_validation/SpringBootValidation/controllers/CustomerController.java
@@ -2,17 +2,22 @@ package com.spring_boot_validation.SpringBootValidation.controllers;
 
 import com.spring_boot_validation.SpringBootValidation.pojos.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/customers")
 public class CustomerController {
+
+    @InitBinder
+    public void trimInput(WebDataBinder webDataBinder){
+        StringTrimmerEditor stringTrimmerEditor=new StringTrimmerEditor(true);
+        webDataBinder.registerCustomEditor(String.class,stringTrimmerEditor);
+    }
 
     @GetMapping("/register")
     public String showCustomerForm(Model theModel){
@@ -27,6 +32,7 @@ public class CustomerController {
         if(bindingResult.hasErrors()){
             return "show-registration";
         }else{
+            System.out.println("Customer FirstName: "+"|"+customer.getFirstName()+"|"+"\n");
             return "submit-registration";
         }
     }
